57473e084cbdc32cf793002344a12e2ad2573a9c
FilipKatana <40831024+FilipKatana@users.noreply.github.com>
2018-08-30 11:58:57 +0200

    Add files via upload

---
+++ GitInit.py
@@ -0,0 +1,35 @@
+
+"""
+©This file has been made by Filip Katana for the purpose
+of its usage in the git project. Any copying or unauthorised
+selling of this file is strictly forbidden by law.
+"""
+
+
+import os
+
+from subprocess import call
+
+
+class private:
+    def hide(folder_path):
+        call(["attrib", "+H", folder_path])
+
+
+    def unhide(folder_path):
+        call(["attrib", "-H", folder_path])
+
+
+"""
+Function takes in no arguments and sets up the hidden folder
+and the staging area folder titled stgarea inside the folder from which the
+program was ran.
+"""
+def init():
+    if os.path.exists(os.path.join(os.getcwd(), ".backups")):
+        return -1
+    os.mkdir(".backups")
+    os.mkdir(os.path.join(os.getcwd(), ".backups", "stgarea"))
+    private.hide(os.path.join(os.getcwd(), ".backups"))
+
+
